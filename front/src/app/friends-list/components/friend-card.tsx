c6f8b227afb723f82b56bbf25e0876816caa01de
Yovanne05 <yovanne.ponnou@gmail.com>
2025-04-21 20:11:57 +0200

    fix: tailwind

--- front/src/app/friends-list/components/friend-card.tsx
+++
@@ -1,8 +0,0 @@
-export default function FriendCard(){
-    return (
-        <div>
-            <p>nom</p>
-            <p>PP Image</p>
-        </div>
-    )
-}
--- front/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function Home() {
-  return (
-      <div>
-          <Link href="/friends-list">page1</Link>
-      </div>
-  );
-}
